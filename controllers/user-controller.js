7b00e6b5a860bbccd6db2816da14d47e7433bb96
Yuvraj Gupta <yuvrajam12@gmail.com>
2023-08-18 08:38:01 +0530

    second commit

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,31 +3,48 @@ import usermodel from "../DB/modules/user-schema.js";
 import { hashing } from "../utils/encrypt.js";
 
 export const userController = {
-  async login(req, res) {
-    const userinfo = req.body;
-
-    const doc = await usermodel.findOne({ email: userinfo.email }).exec();
-    console.log(doc);
-    if (doc && doc._id) {
-      const plainpwd = userinfo.password;
-      const dbpwd = doc.password;
-      if (hashing.matchpassword(plainpwd, dbpwd)) {
-        res.json({ message: "welcome " + doc.name });
-      } else {
-        res.json({ message: "incorrect pwd" });
-      }
-    } 
-    else {
-      res.json({ message: "invalid email" });
+  async login(request, response){
+    const userInfo=request.body
+    const doc=await UserModel.findOne({'email':userInfo.email}).exec()
+    console.log("doc is",doc);
+    if(doc&&doc._id){
+        const plainPassword=userInfo.password
+        const dbPassword=doc.password
+        if(hashing.matchPassword(plainPassword,dbPassword)){
+            response.json({message:'Welcome '+doc.name})
+        }
+        else{
+            response.json({message:"Invalid user Id or password"})
+        }
     }
-    // const userinfo = req.body;
-    // // console.log('request body is ',userinfo);
-    // if (userinfo.id == userinfo.username)
-    //   res.json({ message: "welcome " + userinfo.username });
-    // else {
-    //   res.json({ message: "invalid user" });
-    // }
-  },
+    else{
+        response.json({message:"Invalid user Id or password"})
+    }},
+  // async login(req, res) {
+  //   const userinfo = req.body;
+
+  //   const doc = await usermodel.findOne({ email: userinfo.email }).exec();
+  //   console.log(doc);
+  //   if (doc && doc._id) {
+  //     const plainpwd = userinfo.password;
+  //     const dbpwd = doc.password;
+  //     if (hashing.matchpassword(plainpwd, dbpwd)) {
+  //       res.json({ message: "welcome " + doc.name });
+  //     } else {
+  //       res.json({ message: "incorrect pwd" });
+  //     }
+  //   } 
+  //   else {
+  //     res.json({ message: "invalid email" });
+  //   }
+  //   // const userinfo = req.body;
+  //   // // console.log('request body is ',userinfo);
+  //   // if (userinfo.id == userinfo.username)
+  //   //   res.json({ message: "welcome " + userinfo.username });
+  //   // else {
+  //   //   res.json({ message: "invalid user" });
+  //   // }
+  // },
   profile(req, res) {
     const userName = req.params;
     console.log("all params", userName);
