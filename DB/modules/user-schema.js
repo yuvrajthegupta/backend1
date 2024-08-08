8e07223256ea06e31bacc8f45bc72638ed82344d
Yuvraj Gupta <96349681+yuvrajthegupta@users.noreply.github.com>
2024-08-08 14:40:20 +0530

    Update user-schema.js

--- DB/modules/user-schema.js
+++ DB/modules/user-schema.js
@@ -6,7 +6,8 @@ const userschema = new schema({
   'email': { type: SchemaTypes.String, required: true, unique: true },
   'password': { type: SchemaTypes.String, required: true, minLength : 8,maxLength : 100 },
   'name': { type: SchemaTypes.String, required: true },
-  'phone': { type: SchemaTypes.String }
+  'phone': { type: SchemaTypes.String },
+  'info': { type: SchemaTypes.String }
 });
 const usermodel = mongoose.model("user", userschema);
 export default usermodel;
